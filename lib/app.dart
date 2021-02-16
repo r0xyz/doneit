54a3ac8ada4d404fb29d5c7ee1482ff688351cab
a <null@github.com>
2021-02-16 22:11:01 +0900

    更新

--- lib/app.dart
+++ lib/app.dart
@@ -7,16 +7,19 @@ import 'package:doneit/task.dart';
 
 // TODO: ツイッター連携
 // TODO: パーセントで後ろの色を変える
+// TODO: タップで新しいページを作る
+// TODO: だんだんキツくなるタスクの作成
+// TODO: 確認の通知
 
 class App extends StatelessWidget {
-  final _title = "Done It";
+  final _title = "鉄の掟";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.grey,
       ),
       home: TaskListWidget(
         title: _title,
@@ -37,18 +40,18 @@ class TaskListWidget extends StatefulWidget {
 class _TaskListWidget extends State<TaskListWidget> {
   var _tasks = [
     Task(
-      title: "腹筋を10回する",
+      title: "腕立てをする",
       period: Duration(days: 1),
-      volume: 500,
+      volume: 50,
     ),
     Task(
-      title: "散歩をする",
+      title: "スクワットをする",
       period: Duration(days: 1),
-      volume: 365,
+      volume: 36,
     ),
     Task(
-      title: "飲み物を飲む",
-      period: Duration(seconds: 5),
+      title: "腹筋をする",
+      period: Duration(days: 1),
       volume: 3,
     ),
   ];
@@ -58,7 +61,7 @@ class _TaskListWidget extends State<TaskListWidget> {
   void initState() {
     super.initState();
 
-    _timer = Timer.periodic(Duration(seconds: 10), (timer) {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       _tasks.forEach((task) {
         setState(() {
           task.dateFor(DateTime.now()).update();
@@ -107,6 +110,13 @@ class _TaskListWidget extends State<TaskListWidget> {
             actionPane: SlidableDrawerActionPane(),
             actionExtentRatio: 0.25,
             secondaryActions: [
+              // 編集ボタン
+              IconSlideAction(
+                caption: '編集',
+                color: Colors.blue,
+                icon: Icons.edit,
+                onTap: () {},
+              ),
               // 削除ボタン
               IconSlideAction(
                 caption: '削除',

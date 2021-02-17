c86e413f123be6ce30da55f1eaf2cb7cfe080e1b
Diamond Cat <uyuuki0919@icloud.com>
2021-02-17 12:22:20 +0900

    renew task information widgets

--- lib/widget/task_list.dart
+++ lib/widget/task_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 import 'package:doneit/util/task.dart';
 import "package:doneit/util/duration_format.dart";
@@ -11,9 +12,9 @@ class TaskListWidget extends StatefulWidget {
 class _TaskListWidget extends State<TaskListWidget> {
   final List<Task> _tasks = [
     Task("腹筋100回", Duration(days: 1), 20),
-    Task("腕立て", Duration(days: 1), 50),
-    Task("スクワット", Duration(days: 1), 36),
-    Task("腹筋", Duration(seconds: 5), 3),
+    Task("腕立て", Duration(days: 1), 50, [0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1]),
+    Task("スクワット", Duration(days: 1), 36, [0, 1, 1, 1, 1]),
+    Task("腹筋", Duration(seconds: 5), 3, [1, 0, 1]),
   ];
 
   @override
@@ -56,64 +57,81 @@ class TaskWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text("掟の詳細"),
       ),
-      body: Column(children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.blue,
-            child: Center(child: Text("${task.title} を ${DurationFormat(task.duration.period, every: true)} やります！", style: Theme.of(context).textTheme.headline6)),
-          ),
+      body: ListView(children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.3,
+          color: Colors.teal,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text("${task.title} を ${DurationFormat(task.duration.period, every: true)} やります！", style: Theme.of(context).textTheme.headline6),
+          ]),
         ),
-        Expanded(
-          flex: 1,
-          child: Column(children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Row(children: [
-                  Expanded(
-                    flex: 2,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("${DurationFormat(task.duration.period, every: true)} x ${task.duration.repeat} 連続")]),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("計 ${DurationFormat(task.duration.total)}")]),
-                  )
-                ]),
-              ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text("${DurationFormat(task.duration.period, every: true)} x ${task.duration.repeat} 連続 = 計 ${DurationFormat(task.duration.total)}"),
+          ]),
+        ),
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            LinearPercentIndicator(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              lineHeight: 30,
+              animation: true,
+              animationDuration: 3000,
+              percent: task.progress.completedRate,
+              center: Text("${(task.progress.completedRate * 100).toInt()}% 完了"),
+              trailing: Text("${task.progress.completedCount} / ${task.progress.length}"),
+              linearStrokeCap: LinearStrokeCap.roundAll,
+              progressColor: Colors.blue,
+              backgroundColor: Theme.of(context).splashColor,
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.red[600],
-                child: Row(children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("開始")]),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("${task.duration.startedAt}")]),
-                  )
-                ]),
-              ),
+          ]),
+        ),
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CircularPercentIndicator(
+              radius: 120,
+              lineWidth: 15,
+              animation: true,
+              animationDuration: 4000,
+              percent: task.progress.succeedRate,
+              center: Text("${(task.progress.succeedRate * 100).toInt()}% 達成"),
+              footer: Text("${task.progress.succeedCount} / ${task.progress.failedCount}"),
+              progressColor: Colors.green,
+              backgroundColor: Theme.of(context).splashColor,
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.green[600],
-                child: Row(children: [
-                  Expanded(
-                    flex: 1,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("終了予定")]),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("${task.duration.endsAt}")]),
-                  )
-                ]),
+          ]),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Row(children: [
+              Expanded(
+                flex: 1,
+                child: Column(children: [Text("開始")]),
               ),
-            ),
+              Expanded(
+                flex: 2,
+                child: Column(children: [Text("${task.duration.startedAt}")]),
+              )
+            ]),
+          ]),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Row(children: [
+              Expanded(
+                flex: 1,
+                child: Column(children: [Text("終了予定")]),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(children: [Text("${task.duration.endsAt}")]),
+              )
+            ]),
           ]),
         ),
       ]),
